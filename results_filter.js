5aef9558138650951f9987f15f18bd7c24289b1c
Sarah German <sgerman@smm.org>
2011-05-19 23:02:10 -0500

    standardized variable names; fixed form submit function

--- results_filter.js
+++ results_filter.js
@@ -1,28 +1,28 @@
 Drupal.behaviors.results_filterBehavior = function (context) {
 
   // create an array for class names
-  var $types = [];
+  var types = [];
 
   // set a variable to represent the search results
   var $results = $('dl.search-results div').not('#search-tools').not('#search-tools div');
 
   // loop through results divs and add their class names to the $types array
   $($results).each(function() {
-    $types.push(this.className);
+    types.push(this.className);
   });
  
   // filter the array of classes down to just the unique ones
-  var $links = $types.filter(function(itm, i, $types) {
-    return i == $types.indexOf(itm);
+  var links = types.filter(function(itm, i, types) {
+    return i == types.indexOf(itm);
   });
   
   // Build and output the filter links
-  var $options = '';
-  $.each($links, function(index, value) {   
-    $label = value.replace("_", " "); // convert underscores to spaces for the link text
-    $options += '<option value="'+ value +'">' + $label + '</option>'; // markup for the link
+  var options = '';
+  $.each(links, function(index, value) {   
+    var label = value.replace("_", " "); // convert underscores to spaces for the link text
+    options += '<option value="'+ value +'">' + label + '</option>'; // markup for the link
   }); 
-  $($options).appendTo('#types'); // put the options into the select list
+  $(options).appendTo('#types'); // put the options into the select list
 
   // Filter search results
   $('#types').change(function() {
@@ -33,12 +33,10 @@ Drupal.behaviors.results_filterBehavior = function (context) {
 
     $query = $('input#edit-keys').val(); // store the original query
     $adv_query = $(this).val(); // get the desired type by looking at the selected option
-    $adv_query_type = ''; // TODO: We need the machine name of the content type, not the label.
 
-    $('input#edit-keys').val($query + ' type:' + $adv_query); // populate the search form with the adv query
-     
-    $('input#edit-submit').trigger('click'); // submit the form with the new query
-    $('#edit-submit').attr('disabled', 'disabled'); // disable the button while the results load    
+    $('input#edit-keys').val($query + ' type:' + $adv_query); // populate the search form with the adv query 
+    $('#search-form').submit(); // submit the form with the new query
+    $('#edit-submit').attr('disabled', 'disabled'); // disable the button while the results load        
 
   });
 
